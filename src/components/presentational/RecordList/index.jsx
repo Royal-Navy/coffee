518a13f79251a163a82edbb34e6735ba42c29ee5
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-10-03 15:58:42 +0100

    Add RecordList state styles

--- src/components/presentational/RecordList/index.jsx
+++ src/components/presentational/RecordList/index.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Avatar, Badge } from '@royalnavy/react-component-library'
-import { IconCheckCircle } from '@royalnavy/icon-library'
+import { IconCheckCircle, IconWarning, IconChat } from '@royalnavy/icon-library'
 
 import './RecordList.scss'
 
 export const RecordList = props => {
+  const [active, setActive] = useState(null)
+
   return (
     <section className="record-list">
       <table className="record-list__table">
         <tbody>
-          <tr className="record-list__row">
+          <tr
+            className="record-list__row"
+            onClick={({ target }) => setActive(target.getAttribute('id'))}
+            data-id="1"
+          >
             <td>
               <div className="record-list__name">
                 <Avatar initials="AR" dark />
@@ -23,15 +29,50 @@ export const RecordList = props => {
               </Badge>
             </td>
             <td>
-              <span>@adam</span>
+              <span className="record-list__tweet">
+                <IconChat />
+                @adam
+              </span>
             </td>
             <td>
-              <span>&pound;4 /month</span>
+              <span className="record-list__cost">
+                &pound;4 <span>/month</span>
+              </span>
             </td>
             <td>
-              <span className="record-list__status">
+              <span className="record-list__status is-paid">
                 <IconCheckCircle />
                 Paid Up
+              </span>
+            </td>
+          </tr>
+          <tr className="record-list__row is-active">
+            <td>
+              <div className="record-list__name">
+                <Avatar initials="AR" dark />
+                <span>Adam Rooney</span>
+              </div>
+            </td>
+            <td>
+              <Badge color="danger" colorVariant="faded">
+                Paused
+              </Badge>
+            </td>
+            <td>
+              <span className="record-list__tweet">
+                <IconChat />
+                @adam
+              </span>
+            </td>
+            <td>
+              <span className="record-list__cost">
+                &pound;4 <span>/month</span>
+              </span>
+            </td>
+            <td>
+              <span className="record-list__status is-overdue">
+                <IconWarning />
+                PAYMENTS DUE
               </span>
             </td>
           </tr>
@@ -48,163 +89,18 @@ export const RecordList = props => {
               </Badge>
             </td>
             <td>
-              <span>@adam</span>
-            </td>
-            <td>
-              <span>&pound;4 /month</span>
-            </td>
-            <td>
-              <span className="record-list__status">
-                <IconCheckCircle />
-                Paid Up
+              <span className="record-list__tweet">
+                <IconChat />
+                @adam
               </span>
             </td>
-          </tr>
-          <tr className="record-list__row">
             <td>
-              <div className="record-list__name">
-                <Avatar initials="AR" dark />
-                <span>Adam Rooney</span>
-              </div>
-            </td>
-            <td>
-              <Badge color="danger" colorVariant="faded">
-                Paused
-              </Badge>
-            </td>
-            <td>
-              <span>@adam</span>
-            </td>
-            <td>
-              <span>&pound;4 /month</span>
-            </td>
-            <td>
-              <span className="record-list__status">
-                <IconCheckCircle />
-                Paid Up
+              <span className="record-list__cost">
+                &pound;4 <span>/month</span>
               </span>
             </td>
-          </tr>
-          <tr className="record-list__row">
             <td>
-              <div className="record-list__name">
-                <Avatar initials="AR" dark />
-                <span>Adam Rooney</span>
-              </div>
-            </td>
-            <td>
-              <Badge color="danger" colorVariant="faded">
-                Paused
-              </Badge>
-            </td>
-            <td>
-              <span>@adam</span>
-            </td>
-            <td>
-              <span>&pound;4 /month</span>
-            </td>
-            <td>
-              <span className="record-list__status">
-                <IconCheckCircle />
-                Paid Up
-              </span>
-            </td>
-          </tr>
-          <tr className="record-list__row">
-            <td>
-              <div className="record-list__name">
-                <Avatar initials="AR" dark />
-                <span>Adam Rooney</span>
-              </div>
-            </td>
-            <td>
-              <Badge color="danger" colorVariant="faded">
-                Paused
-              </Badge>
-            </td>
-            <td>
-              <span>@adam</span>
-            </td>
-            <td>
-              <span>&pound;4 /month</span>
-            </td>
-            <td>
-              <span className="record-list__status">
-                <IconCheckCircle />
-                Paid Up
-              </span>
-            </td>
-          </tr>
-          <tr className="record-list__row">
-            <td>
-              <div className="record-list__name">
-                <Avatar initials="AR" dark />
-                <span>Adam Rooney</span>
-              </div>
-            </td>
-            <td>
-              <Badge color="danger" colorVariant="faded">
-                Paused
-              </Badge>
-            </td>
-            <td>
-              <span>@adam</span>
-            </td>
-            <td>
-              <span>&pound;4 /month</span>
-            </td>
-            <td>
-              <span className="record-list__status">
-                <IconCheckCircle />
-                Paid Up
-              </span>
-            </td>
-          </tr>
-          <tr className="record-list__row">
-            <td>
-              <div className="record-list__name">
-                <Avatar initials="AR" dark />
-                <span>Adam Rooney</span>
-              </div>
-            </td>
-            <td>
-              <Badge color="danger" colorVariant="faded">
-                Paused
-              </Badge>
-            </td>
-            <td>
-              <span>@adam</span>
-            </td>
-            <td>
-              <span>&pound;4 /month</span>
-            </td>
-            <td>
-              <span className="record-list__status">
-                <IconCheckCircle />
-                Paid Up
-              </span>
-            </td>
-          </tr>
-          <tr className="record-list__row">
-            <td>
-              <div className="record-list__name">
-                <Avatar initials="AR" dark />
-                <span>Adam Rooney</span>
-              </div>
-            </td>
-            <td>
-              <Badge color="danger" colorVariant="faded">
-                Paused
-              </Badge>
-            </td>
-            <td>
-              <span>@adam</span>
-            </td>
-            <td>
-              <span>&pound;4 /month</span>
-            </td>
-            <td>
-              <span className="record-list__status">
+              <span className="record-list__status is-paid">
                 <IconCheckCircle />
                 Paid Up
               </span>
