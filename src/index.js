37db9fca947009e3647c412c946355a33dc9b3ac
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-10-03 09:29:13 +0100

    Redirect on page load

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '@royalnavy/css-framework'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import { Subscriptions } from './pages/Subscriptions'
@@ -14,7 +14,10 @@ const store = createAppStore()
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Route path="/(subscriptions/:id?)" component={Subscriptions} />
+      <Route path="/" exact>
+        <Redirect to="/subscriptions" />
+      </Route>
+      <Route path="/subscriptions/:id?" component={Subscriptions} />
       <Route path="/costs/:id?" component={Costs} />
     </Router>
   </Provider>,
