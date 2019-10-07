b4a33f9e347042f83236e5614af149727d62a98b
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-10-07 12:14:49 +0100

    Create and style subscriber form

--- src/components/presentational/SubscribeModal/index.jsx
+++ src/components/presentational/SubscribeModal/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
+import { Field, Formik, Form } from 'formik'
+
 import { HeadingIcon } from '../HeadingIcon'
 
 import { Modal } from '@royalnavy/react-component-library'
+import { Formik as FormikComponents } from '@royalnavy/react-component-library'
 import { IconPerson, IconCreditCard } from '@royalnavy/icon-library'
 
 import './SubscribeModal.scss'
@@ -25,6 +28,23 @@ export const SubscribeModal = withRouter(({ history }) => {
     variant: 'tertiary',
     color: 'danger',
   }
+
+  const initialValues = {}
+  const onSubmit = () => {}
+
+  const frequencyOptions = [
+    { label: 'Daily', value: 'daily' },
+    { label: 'Weekly', value: 'weekly' },
+    { label: 'Bi-weekly', value: 'bi-weekly' },
+    { label: 'Monthly', value: 'monthly' },
+  ]
+
+  const paymentTypeOptions = [
+    { label: 'Direct Debit', value: 'Direct Debit' },
+    { label: 'Cash', value: 'Cash' },
+  ]
+
+  const { TextInput, Select, NumberInput, Switch } = FormikComponents
 
   return (
     <Modal
@@ -50,7 +70,99 @@ export const SubscribeModal = withRouter(({ history }) => {
           <p>Manage the size and frequency of the Subscriber's payments.</p>
         </div>
       </header>
-      <div className="subscribe-modal__body"></div>
+      <div className="subscribe-modal__body">
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          render={({ setFieldValue }) => {
+            return (
+              <Form
+                style={{ display: 'flex' }}
+                className="subscribe-modal__form"
+              >
+                <div style={{ width: '50%', marginRight: '1rem' }}>
+                  <div style={{ display: 'flex' }}>
+                    <div>
+                      <Field
+                        component={TextInput}
+                        name="firstName"
+                        label="First Name"
+                      />
+                    </div>
+                    <div style={{ paddingLeft: '.5rem' }}>
+                      <Field
+                        style={{ paddingLeft: '.5rem' }}
+                        component={TextInput}
+                        name="lastName"
+                        label="Last Name"
+                      />
+                    </div>
+                  </div>
+                  <Field component={TextInput} name="email" label="Email" />
+                  <div style={{ display: 'flex' }}>
+                    <div>
+                      <Field
+                        component={TextInput}
+                        name="contactName"
+                        label="Contact Number"
+                      />
+                    </div>
+                    <div style={{ paddingLeft: '.5rem' }}>
+                      <Field
+                        component={TextInput}
+                        name="handle"
+                        label="Slack Username"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    width: '50%',
+                  }}
+                >
+                  <Field
+                    component={NumberInput}
+                    min={1}
+                    max={10}
+                    name="amount"
+                    label="Subscription"
+                  />
+                  <Field
+                    name="frequency"
+                    component={Switch}
+                    options={frequencyOptions}
+                    onChange={event => {
+                      setFieldValue('frequency', event.target.value)
+                      console.log(event)
+                    }}
+                  />
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <div>
+                      <Field
+                        component={Select}
+                        options={paymentTypeOptions}
+                        name="paymentType"
+                        label="Payment Type"
+                      />
+                    </div>
+                    <div style={{ paddingLeft: '.5rem' }}>
+                      <Field
+                        component={TextInput}
+                        name="startDate"
+                        label="Start Date"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Form>
+            )
+          }}
+        />
+      </div>
     </Modal>
   )
 })
